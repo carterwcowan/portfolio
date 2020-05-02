bc9d31c213d05201fadaedcf3c8a7de3af4355e8
Carter Cowan <carterwcowan@gmail.com>
2020-05-02 12:57:22 -0500

    update starter

--- src/components/hero.js
+++ src/components/hero.js
@@ -12,8 +12,8 @@ export default ({ data }) => (
     />
     <div className={styles.heroDetails}>
       <h3 className={styles.heroHeadline}>{data.name}</h3>
-      <p className={styles.heroTitle}>{data.title}</p>
-      <p>{data.shortBio.shortBio}</p>
+      {/* <p className={styles.heroTitle}>{data.title}</p>
+      <p>{data.shortBio.shortBio}</p> */}
     </div>
   </div>
 )
