12e8b94309769e6cc2bcdc8c7481a6b4e4e947fe
Carter Cowan <carterwcowan@gmail.com>
2020-05-25 13:13:04 -0500

    update layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,27 +12,108 @@ class RootIndex extends React.Component {
     const posts = get(this, 'props.data.allContentfulBlogPost.edges')
     const nav = get(this, 'props.data.allContentfulNavigation.edges')
     const [author] = get(this, 'props.data.allContentfulPerson.edges')
+    this.onMenuItemClick = this.onMenuItemClick.bind(this)
 
     return (
       <Layout location={this.props.location} showNav={nav.showNav}>
-        <div style={{ background: '#fff' }}>
+        <div style={{ background: '#121212' }}>
           <Helmet title={siteTitle} />
-          <Hero data={author.node} />
-          <div className="wrapper">
-            <h2 className="section-headline">Recent articles</h2>
-            <ul className="article-list">
-              {posts.map(({ node }) => {
-                return (
-                  <li key={node.slug}>
-                    <ArticlePreview article={node} />
-                  </li>
-                )
-              })}
-            </ul>
+          <div className="app-wrapper">
+            <div className="menu-wrapper">
+              <div className="menu-container">
+                <h4>
+                  <a data-target="about" onClick={this.onMenuItemClick}>
+                    About
+                  </a>
+                </h4>
+                <h4>
+                  <a data-target="experience" onClick={this.onMenuItemClick}>
+                    Experience
+                  </a>
+                </h4>
+                <h4>
+                  <a data-target="blog" onClick={this.onMenuItemClick}>
+                    Blog
+                  </a>
+                </h4>
+                <h4>
+                  <a data-target="contact" onClick={this.onMenuItemClick}>
+                    Contact
+                  </a>
+                </h4>
+              </div>
+            </div>
+            <div className="content-wrapper">
+              <div className="page-content">
+                <h1>Carter Cowan</h1>
+                <p className="meta">Fullstack Engineer</p>
+                <p className="meta">Austin, TX</p>
+              </div>
+              <div className="page-content">
+                <h2 id="about">About</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </p>
+              </div>
+              <div className="page-content">
+                <h2 id="experience">Experience</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </p>
+              </div>
+              <div className="page-content">
+                <h2 id="blog">Blog</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </p>
+              </div>
+              <div className="page-content">
+                <h2 id="contact">Contact</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </Layout>
     )
+  }
+
+  onMenuItemClick({ target }) {
+    console.log(target)
+    document.getElementById(target.dataset.target).scrollIntoView({
+      behavior: 'smooth',
+      block: 'center'
+    })
   }
 }
 
